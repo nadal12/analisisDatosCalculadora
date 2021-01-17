c6e76033672b2e5fbe6b34c8b9018567e6cd3650
nadal12 <nadal12@hotmail.com>
2021-01-17 13:06:31 +0100

    Filtrar i preparar los datos para el análisis.

--- analisisDatos.R
+++ analisisDatos.R
@@ -1,1 +1,21 @@
-data <- readcsv("dades.xlsx")
+library(readr)
+
+#Leer datos i mostrar su estructura. 
+data <- read_delim("dades.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+str(data)
+
+#Quitar valores NA
+data <- na.omit(data)
+
+#Convertir las columnas "grup" y "esquerra/dreta" a factor.
+data$`Grup (BM o MB)` <- factor(data$`Grup (BM o MB)`)
+data$`Esquerra/Dreta` <- factor(data$`Esquerra/Dreta`)
+
+summary(data)
+
+#Seleccionar columnas necesarias para este análisis. 
+selectedData <- data[, c(-2:-4)]
+str(selectedData)
+
+#Convertir a long format. 
+library(tidyr)
